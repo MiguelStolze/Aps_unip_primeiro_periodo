2b784fcdd16762a8290012295f6e2129e152eedd
Miguel Stolze <miguelstolze.ms@gmail.com>
2024-04-25 18:14:57 -0400

    inseri img da ODS, adicionei o estilo e as funções para selecionar cada item da lista

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let menuToggle = document.querySelector(".toggle");
 let menu = document.querySelector(".menu");
 let imgToggle = document.querySelector(".toggle img");
-
-let menuItems = document.querySelectorAll(".menu.active li");
-
-menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.classList.toggle("active");
-  });
-});
-
+let itensMenu = document.querySelectorAll(".menu-item");
+let itemSelecionado = null;
 function alterarMenu() {
   menuToggle.classList.toggle("active");
   menu.classList.toggle("active");
   imgToggle.classList.toggle("active");
+  removerSeleção();
+}
+
+function secLi(elemento) {
+  removerSeleção();
+  elemento.classList.add("activeLi");
+  itemSelecionado = elemento;
+}
+
+function removerSeleção() {
+  if (itemSelecionado !== null) {
+    itemSelecionado.classList.remove("activeLi");
+  }
 }
